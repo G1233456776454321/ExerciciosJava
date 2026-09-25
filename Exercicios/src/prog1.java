4c250c3e3c1272bdb8528042d63002b44eff182d
G1233456776454321 <gabriel.b.lima17@aluno.senai.br>
2026-09-24 21:48:38 -0300

    Add files via upload

---
+++ Exercicios/src/prog1.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class prog01 {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        String palavra;
+        int contador = 0;
+        boolean v = true;
+        boolean verificadorcontagem = false;
+
+        do {
+
+
+            do {
+                contador++;
+                verificadorcontagem = false;
+                System.out.printf("\nTentativa %d \n", contador);
+                System.out.println("Digite a palavra-chave: ");
+                palavra = leitor.next();
+                contador = contador + 1;
+                if(contador==7){
+                    verificadorcontagem = true;
+                    break;
+                }
+            } while (!palavra.equalsIgnoreCase("banana"));
+            if (verificadorcontagem==false){
+                contador--;
+                System.out.printf("Você conseguiu em %d tentativa \n", contador);
+            }
+            else {
+                System.out.println("Você não conseguiu");
+                System.out.printf("A palavra era: %s", palavra);
+            }
+        }while (v==true);
+    }
+}
